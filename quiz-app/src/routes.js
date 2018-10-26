65ba9e1ee4d02faf6095330fa419c1d8e5c50107
thaipd <thaicnt50@gmail.com>
2018-10-26 14:37:46 +0700

    - first commit

---
+++ quiz-app/src/routes.js
@@ -0,0 +1,39 @@
+import HomePage from './pages/home.vue';
+import AboutPage from './pages/about.vue';
+import FormPage from './pages/form.vue';
+import DynamicRoutePage from './pages/dynamic-route.vue';
+import NotFoundPage from './pages/not-found.vue';
+
+import PanelLeftPage from './pages/panel-left.vue';
+import PanelRightPage from './pages/panel-right.vue';
+
+export default [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/panel-left/',
+    component: PanelLeftPage,
+  },
+  {
+    path: '/panel-right/',
+    component: PanelRightPage,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '/form/',
+    component: FormPage,
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: DynamicRoutePage,
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
